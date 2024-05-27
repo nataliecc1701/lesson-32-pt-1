e4a625a20d3d50068501fd4efe3555b47be40579
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-05-27 14:38:19 -0700

    added /all route

--- calc.js
+++ calc.js
@@ -64,6 +64,25 @@ app.get("/mode", function(request, response) {
     return response.json(ans)
 })
 
+app.get("/all", function(request,response) {
+    const ans = {operation: "all"};
+    let nums;
+    try {
+        nums = calcs.stringToNumericArray(request.query.nums)
+    }
+    catch(error) {
+        let message = "missing required parameter: nums"
+        if (error.message) {
+            message = `${error.message} is not a number`
+        }
+        throw new ExpressError(message, 400)
+    }
+    ans.mean = calcs.arrayMean(nums);
+    ans.median = calcs.arrayMedian(nums);
+    ans.mode = calcs.arrayMode(nums);
+    return response.json(ans)
+})
+
 // handle errors
 app.use(function(err, request, response, next) {
     const status = err.status || 500;
