722545c3e8679d1d189dc2b927c05a31f0160100
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-05-27 14:35:27 -0700

    all tests in place

--- calc-helpers.test.js
+++ calc-helpers.test.js
@@ -17,7 +17,37 @@ describe("tests for the string parser", function() {
 
 describe("tests for the mean calculator", function() {
     test("returns the mean of an array", function() {
-        const testArr = [3,4,5,6]
+        const testArr = [3,4,5,6];
         expect(calcs.arrayMean(testArr)).toStrictEqual(4.5)
+    })
+})
+
+describe("tests for the median calculator", function() {
+    test("returns the median of an array of odd length", function() {
+        const testArr = [4,5,3];
+        expect(calcs.arrayMedian(testArr)).toStrictEqual(4);
+    })
+    test("averages the two middle values of an array of even length", function() {
+        const testArr = [9,6,1,5];
+        expect(calcs.arrayMedian(testArr)).toStrictEqual(5.5);
+    })
+})
+
+describe("tests for the mode calculator", function() {
+    test("returns an array of modes", function() {
+        const testArr = [4,5,3,5];
+        expect(calcs.arrayMode(testArr)).toBeInstanceOf(Array)
+    })
+    test("returns the single mode of an array", function() {
+        const testArr = [4,5,3,5];
+        expect(calcs.arrayMode(testArr)).toEqual([5])
+    })
+    test("returns multiple modes if they exist", function() {
+        const testArr = [7,5,3,2,3,7,9,3,4,7,5]
+        expect(calcs.arrayMode(testArr)).toEqual([3,7])
+    })
+    test("returns an empty array if no inputs repeat", function() {
+        const testArr = [1,2,3,4,5,6]
+        expect(calcs.arrayMode(testArr)).toEqual([])
     })
 })
